b4ecf5c38b17d1affa827e2bf38d78303f183082
Reid <Reid.Finn@yahoo.com>
2021-01-14 11:50:41 -0600

    commit tree fc73ac9078d4a485168ffff39855a80431c524d6

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,7 @@
 class StudentsController < ApplicationController
-  
+
+  before_action :find_student, only: :show
+
   def index
     @students = Student.all
   end
@@ -8,4 +10,17 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
+  def new
+  end
+
+  def create
+  @student = Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
+  redirect_to student_path(@student)
+  end
+
+  private
+
+  def find_student
+    @student = Student.find(params[:id])
+  end
 end
